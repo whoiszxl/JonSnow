4cb7ddc791f4710b58901821b8247ecd7a3d1e97
whoiszxl <whoiszxl@gmail.com>
2018-01-11 23:09:01 +0800

    页面前端静态化

--- code/seckill-5/src/main/java/com/whoiszxl/seckill/controller/GoodsController.java
+++ code/seckill-5/src/main/java/com/whoiszxl/seckill/controller/GoodsController.java
@@ -19,8 +19,10 @@ import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import com.whoiszxl.seckill.entities.SeckillUser;
 import com.whoiszxl.seckill.redis.GoodsKey;
 import com.whoiszxl.seckill.redis.RedisService;
+import com.whoiszxl.seckill.result.Result;
 import com.whoiszxl.seckill.service.IGoodsService;
 import com.whoiszxl.seckill.service.IUserService;
+import com.whoiszxl.seckill.vo.GoodsDetailVo;
 import com.whoiszxl.seckill.vo.GoodsVo;
 
 @Controller
@@ -67,9 +69,9 @@ public class GoodsController {
     	return html;
 	}
 
-	@RequestMapping(value="/to_detail/{goodsId}",produces="text/html")
+	@RequestMapping(value="/to_detail2/{goodsId}",produces="text/html")
 	@ResponseBody
-	public String detail(HttpServletRequest request, HttpServletResponse response,
+	public String detail2(HttpServletRequest request, HttpServletResponse response,
 			Model model, SeckillUser user, @PathVariable("goodsId") long goodsId) {
 		model.addAttribute("user", user);
 		
@@ -112,5 +114,34 @@ public class GoodsController {
     	}
 		return html;
 	}
+	
+	@RequestMapping(value="/detail/{goodsId}")
+    @ResponseBody
+    public Result<GoodsDetailVo> detail(HttpServletRequest request, HttpServletResponse response, Model model,SeckillUser user,
+    		@PathVariable("goodsId")long goodsId) {
+    	GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
+    	long startAt = goods.getStartDate().getTime();
+    	long endAt = goods.getEndDate().getTime();
+    	long now = System.currentTimeMillis();
+    	int miaoshaStatus = 0;
+    	int remainSeconds = 0;
+    	if(now < startAt ) {//秒杀还没开始，倒计时
+    		miaoshaStatus = 0;
+    		remainSeconds = (int)((startAt - now )/1000);
+    	}else  if(now > endAt){//秒杀已经结束
+    		miaoshaStatus = 2;
+    		remainSeconds = -1;
+    	}else {//秒杀进行中
+    		miaoshaStatus = 1;
+    		remainSeconds = 0;
+    	}
+    	GoodsDetailVo vo = new GoodsDetailVo();
+    	vo.setGoods(goods);
+    	vo.setUser(user);
+    	vo.setRemainSeconds(remainSeconds);
+    	vo.setMiaoshaStatus(miaoshaStatus);
+    	return Result.success(vo);
+    }
+    
 
 }
